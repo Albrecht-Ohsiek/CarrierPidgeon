92055890757ac37abf5e2ac4b715c149b3f5a995
Fenrir-23 <germangeneral2301@gmail.com>
2023-08-23 21:52:19 +0200

    Node Interface

---
+++ Pathfinding/AStar_Algorithm/Business Layer/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AStar_Algorithm.Business_Layer
+{
+    internal class Node : INode
+    {
+        public Node(int start, int end, bool accessible) {
+        
+        }
+
+        public bool accessible { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double gCost { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double hCost { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double fCost { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
