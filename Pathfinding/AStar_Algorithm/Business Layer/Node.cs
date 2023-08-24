097d480a73e7a70c919a4afca52ed99108fc505d
Albrecht Ohsiek <albrecht.ohsiek@eps.ch>
2023-08-24 21:28:04 +0200

    Populate Node Code .. Start, End, Obstacle

--- Pathfinding/AStar_Algorithm/Business Layer/Node.cs
+++ Pathfinding/AStar_Algorithm/Business Layer/Node.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 public class Node : INode
 {
@@ -9,8 +10,9 @@ public class Node : INode
     private int calculatedHCost;
     private string properties;
 
-    public Node(){
-       this.isOccupied = false;
+    public Node()
+    {
+        this.isOccupied = false;
         this.isAccessible = true;
         this.calculatedGCost = 0;
         this.calculatedHCost = 0;
@@ -18,25 +20,88 @@ public class Node : INode
         this.properties = "";
     }
 
-    public Node(bool isOccupied, bool isAccessible, int calculatedFCost, int calculatedGCost, int calculatedHCost, string properties){
-        this.isOccupied=isOccupied;
-        this.isAccessible=isAccessible;
-        this.calculatedFCost=calculatedFCost;
-        this.calculatedGCost=calculatedGCost;
-        this.calculatedHCost=calculatedHCost;
+    public Node(bool isOccupied, bool isAccessible, int calculatedFCost, int calculatedGCost, int calculatedHCost, string properties)
+    {
+        this.isOccupied = isOccupied;
+        this.isAccessible = isAccessible;
+        this.calculatedFCost = calculatedFCost;
+        this.calculatedGCost = calculatedGCost;
+        this.calculatedHCost = calculatedHCost;
         this.properties = properties;
     }
 
     public static Node[,] initNodes(int width, int bredth)
     {
-        Node[,] nodes = new Node[width,bredth];
-        for(int i=0; i < width; i++)
-            for(int j=0; j < bredth; j++)
+        Node[,] nodes = new Node[width, bredth];
+        for (int i = 0; i < width; i++)
+            for (int j = 0; j < bredth; j++)
             {
-                nodes[i,j] = new Node();
+                nodes[i, j] = new Node();
             }
 
         return nodes;
+    }
+
+    public static Node placeObstacle(Node node)
+    {
+        try
+        {
+            if (node.isAccessible)
+            {
+                node.isOccupied = true;
+                node.properties = "obstacle";
+            }
+        }
+        catch (Exception e)
+        {
+            Console.Error.WriteLine(e.Message);
+        }
+
+        return node;
+    }
+
+    public static Node setStart(Node[,] nodes, Node node)
+    {
+        try
+        {
+            foreach (Node item in nodes)
+            {
+                if (item.nodeProperties().ToLower() == "start")
+                {
+                    throw new Exception("Start Node Exists");
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.Error.WriteLine(e.Message);
+            return node;
+        }
+
+        node.properties = "start";
+        return node;
+    }
+
+    public static Node setEnd(Node[,] nodes, Node node)
+    {
+        try
+        {
+            foreach (Node item in nodes)
+            {
+                if (item.nodeProperties().ToLower() == "end")
+                {
+                    throw new Exception("End Node Exists");
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.Error.WriteLine(e.Message);
+            return node;
+        }
+
+        node.properties = "end";
+        return node;
     }
 
     public bool accessible()
--- Pathfinding/AStar_Algorithm/Program.cs
+++ Pathfinding/AStar_Algorithm/Program.cs
@@ -6,6 +6,11 @@ class Program{
         
         // Initialise Map Mesh
         Node[,] nodes = Node.initNodes(5,5);
+        nodes[1,4] = Node.placeObstacle(nodes[1,4]);
+        nodes[4,1]= Node.placeObstacle(nodes[4,1]);
+
+        nodes[2,3] = Node.setStart(nodes, nodes[2,3]);
+        nodes[0,0] = Node.setEnd(nodes, nodes[0,0]);
 
         Display_Console.Display(nodes);
 
