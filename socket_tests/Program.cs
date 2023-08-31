8e28f73cbb2d485dc58871f42d5911b5159bcaca
Albrecht Ohsiek <albrecht.ohsiek@eps.ch>
2023-08-31 10:13:11 +0200

    Created socket_test appliaction

---
+++ socket_tests/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace socket_tests
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string serverIp = "127.0.0.1"; // Replace with your server's IP address
+            int serverPort = 8000; // Replace with your server's port
+
+            TcpClient client = new TcpClient();
+            client.Connect(serverIp, serverPort);
+
+            NetworkStream stream = client.GetStream();
+
+            string requestData = "Hello, server!"; // Your request data
+            byte[] data = Encoding.ASCII.GetBytes(requestData);
+
+            stream.Write(data, 0, data.Length);
+
+            // Receive response (for simplicity, we'll just print it)
+            byte[] responseBytes = new byte[1024];
+            int bytesRead = stream.Read(responseBytes, 0, responseBytes.Length);
+            string response = Encoding.ASCII.GetString(responseBytes, 0, bytesRead);
+            Console.WriteLine("Server response: " + response);
+
+            stream.Close();
+            client.Close();
+        }
+    }
+}
