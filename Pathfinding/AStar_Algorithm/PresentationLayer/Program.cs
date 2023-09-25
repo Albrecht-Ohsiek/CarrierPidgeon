85a110ff5511c2a9230796ddf012eb8930a8a548
Albrecht Ohsiek <albrecht.ohsiek@eps.ch>
2023-09-25 07:34:03 +0200

    Code cleanup

---
+++ Pathfinding/AStar_Algorithm/PresentationLayer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using AStart_Algorithm;
+using Microsoft.AspNetCore.Builder;
+
+namespace AStart_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebApplicationBuilder webApplicationBuilder = WebApplication.CreateBuilder(args);
+
+            WebApplication webApplication = webApplicationBuilder.Build();
+
+            Api_Endpoints.getEnpoints(webApplication);
+
+            webApplication.Run();
+        }
+    }
+}
