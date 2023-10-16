54fb906051a778dd3db8aa0de150758c862809a6
Albrecht Ohsiek <albrecht.ohsiek@eps.ch>
2023-10-16 10:08:05 +0200

    Code cleanup

--- Pathfinding/CarrierPidgeon/src/Services/database.services.cs
+++ Pathfinding/CarrierPidgeon/src/Services/database.services.cs
@@ -2,16 +2,14 @@ using MongoDB.Driver;
 
 public class DatabaseServices
 {
-    private readonly IConfiguration _configuration;
     private readonly IMongoDatabase _database;
 
     public DatabaseServices(IConfiguration configuration)
     {
-        _configuration = configuration;
-
-        var connectionString = _configuration.GetSection("Database:ConnectionString").Value;
+        var connectionString = configuration.GetSection("Database:ConnectionString").Value;
+        var databaseName = configuration.GetSection("Database:DatabaseName").Value;
         var client = new MongoClient(connectionString);
-        _database = client.GetDatabase(_configuration.GetSection("Database:DatabaseName").Value);
+        _database = client.GetDatabase(databaseName);
     }
 
     public IMongoCollection<T> GetCollection<T>(string collectionName)
